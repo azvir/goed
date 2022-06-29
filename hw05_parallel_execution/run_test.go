ce54bb9d1f00b03378428904f764b5ed4b7c8377
Andrei Bondar <andr_bond@mail.ru>
2022-06-29 12:32:20 +0200

    HW5: parallel tasks executions

--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -67,4 +67,32 @@ func TestRun(t *testing.T) {
 		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
 		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
 	})
+
+	t.Run("one worker should handle tasks", func(t *testing.T) {
+		tasksCount := 20
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+
+		for i := 0; i < tasksCount; i++ {
+			err := fmt.Errorf("error from task %d", i)
+			tasks = append(tasks, func() error {
+				return calculateWorkers(&runTasksCount, err)
+			})
+		}
+
+		workersCount := 1
+		maxErrorsCount := 11
+		err := Run(tasks, workersCount, maxErrorsCount)
+
+		require.Truef(t, errors.Is(err, ErrErrorsLimitExceeded), "actual err - %v", err)
+		fmt.Println("runTasksCount: = ", runTasksCount, "workers", workersCount+maxErrorsCount)
+		require.LessOrEqual(t, runTasksCount, int32(workersCount+maxErrorsCount), "extra tasks were started")
+	})
+}
+
+func calculateWorkers(runTasksCount *int32, err error) error {
+	time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
+	atomic.AddInt32(runTasksCount, 1)
+	return err
 }
